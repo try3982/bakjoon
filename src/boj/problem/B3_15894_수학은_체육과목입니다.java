3ef188e0bdd7b7bac19b2295413a920705bbd41b
WonJin <try3982@kakao.com>
2025-10-02 00:21:18 +0900

    -.수학은 체육과목입니다

---
+++ src/boj/problem/B3_15894_수학은_체육과목입니다.java
@@ -0,0 +1,13 @@
+package boj.problem;
+
+import java.util.Scanner;
+
+public class B3_15894_수학은_체육과목입니다 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        Long n = in.nextLong();
+        System.out.println(4L * n);
+        in.close();
+    }
+}
